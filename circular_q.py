b6cca4f80b6730423d57a2f44c5c5bbf13173357
Ravindra <ravindra@turtleyogi.com>
2013-09-21 12:02:38 +0530

    problem description

--- circular_q.py
+++ circular_q.py
@@ -1,3 +1,30 @@
+'''
+Implement a circular buffer of size N. Allow the caller to append, remove and list the contents of the buffer. Implement the buffer to achieve maximum performance for each of the operations.
+
+The new items are appended to the end and the order is retained i.e elements are placed in increasing order of their insertion time. When the number of elements in the list elements exceeds the defined size, the older elements are overwritten.
+ 
+ 
+There are four types of commands.
+ 
+"A"  n -  Append the following n lines to the buffer. If the buffer is full they replace the older entries.
+"R"  n -  Remove first n elements of the buffer. These n elements are the ones that were added earliest among the current elements.
+"L"   - List the elements of buffer in order of their inserting time.
+"Q"  - Quit.  
+ 
+Your task is to execute commands on circular buffer.
+ 
+Input format :
+ 
+First line of input contains N ,  the size of the buffer.
+A n  - append the following n lines to the buffer.
+R n - remove first n elements of the buffer.
+L - list the buffer.
+Q - Quit.
+ 
+Output format :
+ 
+Whenever  L command appears in the input, print the elements of buffer in order of their inserting time. Element that was added first should appear first. 
+'''
 from collections import deque
 
 size = int(raw_input())
--- nlargest.py
+++ nlargest.py
@@ -1,3 +1,15 @@
+'''
+Given an unordered list of N elements, write a function to find the top four elements of the given list. Make sure your function runs in linear time O(N).
+ 
+Input format :
+ 
+First line of input contains N , number of elements in list.
+Next N line , each contains a element.
+ 
+Output format :
+ 
+Print the top four elements of list.
+'''
 import heapq
 
 count = int(raw_input())
